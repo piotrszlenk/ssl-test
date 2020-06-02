1d30e021eb8aea25e1c097da4544a59926d5c4d7
Piotr Szlenk <piotr.szlenk@gmail.com>
2020-06-02 16:17:34 +0200

    Update main

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/piotrszlenk/ssl-test
 
 go 1.13
+
+require (
+	github.com/go-delve/delve v1.4.1 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	github.com/mailgun/godebug v0.0.0-20170609050446-bfb01ae9c266 // indirect
+	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+)
--- pkg/certcheck/certcheck.go
+++ pkg/certcheck/certcheck.go
@@ -1,11 +1,11 @@
 package certcheck
 
 import (
+	"crypto/tls"
 	"fmt"
 
 	"github.com/piotrszlenk/ssl-test/pkg/endpoint"
 	"github.com/piotrszlenk/ssl-test/pkg/logz"
-	"github.com/spacemonkeygo/openssl"
 )
 
 type TestTargets struct {
@@ -32,35 +32,23 @@ func NewTestTargets(epts *endpoint.Endpoints, caPath *string, logger logz.LogHan
 
 func (t *TestTargets) Test(logger logz.LogHandler) {
 	for _, i := range t.Items {
-		logger.Info.Printf("Testing %s\n", i.ept.Fqdn)
+		logger.Info.Printf("Testing %s on port %d\n", i.ept.Fqdn, i.ept.Port)
 		i.testCertificateChain(t.caPath, t.logger)
 	}
 }
 
 func (t *TestTarget) testCertificateChain(caPath *string, logger logz.LogHandler) (*TestTarget, error) {
-	ctx, err := openssl.NewCtx()
-	if err != nil {
-		logger.Error.Printf("Unable to create SSL context for %s\n", t)
-	}
-	err = ctx.LoadVerifyLocations(*caPath, "")
-	if err != nil {
-		logger.Error.Printf("Unable to load CA verify location.\n")
-	}
-
+	cfg := tls.Config{InsecureSkipVerify: true}
 	connStr := fmt.Sprintf("%s:%d", t.ept.Fqdn, t.ept.Port)
-	logger.Info.Printf("Connecting to %s\n", connStr)
-
-	var flags openssl.DialFlags
-	flags = openssl.InsecureSkipHostVerification
-	conn, err := openssl.Dial("tcp", connStr, ctx, flags)
-	logger.Debug.Print(conn)
+	conn, err := tls.Dial("tcp", connStr, &cfg)
 	if err != nil {
-		logger.Error.Print(err)
-		return t, err
+		logger.Error.Printf("Failed to connect: %s\n", err.Error())
+		return t, nil
 	}
-
-	cert, err := conn.PeerCertificate()
-	name, _ := cert.GetSubjectName()
-	logger.Debug.Print(name)
-	return new(TestTarget), nil
+	logger.Info.Printf("Certificate chain sent by the server: %s:%d\n", t.ept.Fqdn, t.ept.Port)
+	for _, cert := range conn.ConnectionState().PeerCertificates {
+		logger.Info.Printf("CN: %s OU: %s ValidNotBefore: %s ValidNotAfter: %s\n", cert.Subject.CommonName, cert.Subject.Organization, cert.NotBefore, cert.NotAfter)
+	}
+	conn.Close()
+	return t, nil
 }
