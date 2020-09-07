5402048519363d3799e2adc1bfabbd76af10ac19
Piotr Szlenk <piotr.szlenk@gmail.com>
2020-09-07 19:51:41 +0200

    Improve error handling

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -37,11 +37,20 @@ func (e *Endpoints) LoadEndpoints() (Endpoints, error) {
 			break
 		}
 		if err != nil {
-			logz.Logger().Error.Fatalln("Unable to read record.")
+			logz.Logger().Error.Printf("Unable to parse entry: %s", entry)
+			continue
 		}
 		logz.Logger().Debug.Printf("Read record: %s", entry)
-		port, _ := strconv.ParseUint(entry[0], 10, 16)
-		e.addEndpoint(Endpoint{entry[1], port})
+		if len(entry) == 2 {
+			port, err := strconv.ParseUint(entry[0], 10, 16)
+			if err != nil {
+				logz.Logger().Error.Printf("Unable to parse port number from line: %s", entry)
+				continue
+			}
+			e.addEndpoint(Endpoint{entry[1], port})
+		} else {
+			logz.Logger().Error.Printf("Unable to parse entry: %s", entry)
+		}
 	}
 
 	logz.Logger().Debug.Printf("Loaded %d records from %s.", len(e.Items), e.path)
